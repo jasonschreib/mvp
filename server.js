4db2209d65df731581b660b1275094275467fc69
jasonschreib <jwschreiber1@gmail.com>
2021-02-02 11:20:17 -0800

    Make successful response to request

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require('express');
 const app = express();
 
 //establish a port
-const port = 3000;
+const port = 8000;
+
+app.use(express.static('client'));
+
+
+app.post('/addEntry', (req, res) => {
+  res.send('Hello world!');
+});
 
 
 
 
 
-
+//listen on this port
+app.listen(port, console.log(`App is listening on port ${port}`));
